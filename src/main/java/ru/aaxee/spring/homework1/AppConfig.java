b10b02c49584eff12e73c2a950e0014e2a1a3444
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2018-12-24 23:06:45 +0300

    Перенес кофигурацию в AppConfig

---
+++ src/main/java/ru/aaxee/spring/homework1/AppConfig.java
@@ -0,0 +1,40 @@
+package ru.aaxee.spring.homework1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
+
+@Configuration
+@ComponentScan
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+
+    @Value("${locale}")
+    private String languageTag;
+
+    @Bean
+    Locale locale() {
+        return Locale.forLanguageTag(languageTag);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+}
--- src/main/java/ru/aaxee/spring/homework1/Main.java
+++ src/main/java/ru/aaxee/spring/homework1/Main.java
@@ -1,46 +1,13 @@
 package ru.aaxee.spring.homework1;
 
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.aaxee.spring.homework1.exception.QuizException;
 
-import java.util.Locale;
-
-@Configurable
-@Configuration
-@ComponentScan
-@PropertySource("classpath:application.properties")
 public class Main {
-
-    @Value("${locale}")
-    private String languageTag;
-
-    @Bean
-    Locale locale() {
-        return Locale.forLanguageTag(languageTag);
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
-    MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setUseCodeAsDefaultMessage(true);
-        return messageSource;
-    }
 
     public static void main(String[] args) throws QuizException {
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(Main.class);
+                new AnnotationConfigApplicationContext(AppConfig.class);
         Application application = context.getBean(Application.class);
         application.run();
     }
